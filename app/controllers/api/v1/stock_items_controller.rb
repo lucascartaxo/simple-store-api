018f287385648095ca6d35eb59a4ccdddd8d9a31
Lucas Cartaxo <lucascartaxo@live.com>
2020-08-09 17:17:08 -0300

    Use BaseController for API

--- app/controllers/api/v1/stock_items_controller.rb
+++ app/controllers/api/v1/stock_items_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::StockItemsController < ApplicationController
+class Api::V1::StockItemsController < Api::V1::BaseController
 
   before_action :set_stock_item, only: [:show, :update, :destroy]
 
--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,4 +1,4 @@
-class  Api::V1::StoresController < ApplicationController
+class  Api::V1::StoresController < Api::V1::BaseController
   before_action :load_stores
   
   def index
