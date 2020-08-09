fe44665805838d998a03f733d3d0cb424ad49b0f
Lucas Cartaxo <lucascartaxo@live.com>
2020-08-09 17:58:22 -0300

    Add coverage for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Product, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :price_cents }
   end
+
+  describe ".price" do
+    let(:product){ create(:product, price_cents: 999123) }
+
+    it "proper user Money price" do
+      expect(product.price).to eq(Money.new(999123, :BRL))
+    end
+  end
 end
